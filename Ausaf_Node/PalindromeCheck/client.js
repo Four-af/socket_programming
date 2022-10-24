a3df813acce0a045f444261a45ff8a656f8a424c
Ausaf1 <ausaf.esrar@gmail.com>
2022-10-24 18:13:36 +0530

    Palindrome check

---
+++ Ausaf_Node/PalindromeCheck/client.js
@@ -0,0 +1,43 @@
+const WebSocket = require("ws");
+const socket = new WebSocket("ws://localhost:8080");
+const readline = require("readline");
+
+//singleton readline interface
+let rl;
+
+/**
+ * @description
+ * 1. socket.on('open') is triggered when the client is connected to the server
+ * 2. socket.on('message') is triggered when the client receives a message from the server
+ * 3. socket.on('error') is triggered when the client encounters an error
+ */
+socket.on("open", () => {
+  console.log("Connected to server");
+
+  sendMsg();
+});
+
+socket.on("error", () => {
+  console.log("Error connecting to server");
+});
+
+/**
+ * @description
+ * Method for sending messages to server
+ */
+const sendMsg = () => {
+  //closing the previous readline
+  if (rl) rl.close();
+
+  //creating a new readline
+  rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question(">> ", (msg) => {
+    socket.send(msg);
+
+    //message trigger for every message sent from client for continuous chat
+    sendMsg();
+  });
+};
---
+++ Ausaf_Node/PalindromeCheck/server.js
@@ -0,0 +1,72 @@
+const { WebSocketServer } = require("ws");
+const wss = new WebSocketServer({ port: 8080 });
+const readline = require("readline");
+
+console.log("Listening on port 8080");
+
+//singleton readline
+let rl;
+
+/**
+ * @description
+ * 1. wss.on('connection') is triggered when the client is connected to the server
+ * 2. ws.on('message') is triggered when the client sends a message to the server
+ * 3. ws.on('close') is triggered when the client is disconnected from the server
+ */
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+
+  //first message trigger
+  sendMsg();
+
+  ws.on("message", (message) => {
+    console.log("\nClient: " + message);
+
+    // check if the message is a palindrome
+    if (isPalindrome(message)) {
+      console.log("Server: " + message + " is a palindrome");
+    } else {
+      console.log("Server: " + message + " is not a palindrome");
+    }
+
+    //message trigger for every message received from client for continuous chat
+    sendMsg();
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+const isPalindrome = (str) => {
+  let len = str.length;
+  for (let i = 0; i <= len / 2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+/**
+ * @description
+ * Method for sending messages to client
+ */
+const sendMsg = () => {
+  //closing the previous readline
+  if (rl) rl.close();
+
+  //creating a new readline
+  rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question(">> ", (msg) => {
+    wss.clients.forEach((client) => {
+      client.send(msg);
+    });
+
+    //message trigger for every message sent from server for continuous chat
+    sendMsg();
+  });
+};
